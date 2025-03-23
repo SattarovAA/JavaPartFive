fabeddb55b73911a3f4f78c372b797b44c957eba
SattarovAA <aa.sattarov@gmail.com>
2025-03-23 16:51:20 +0300

    Инициация репозитория

---
+++ KafkaAuth/AuthServer/src/main/java/org/jpf/SpringKafkaAuthApplication.java
@@ -0,0 +1,11 @@
+package org.jpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaAuthApplication.class, args);
+    }
+}
--- KafkaAuth/ConfirmServer/src/main/java/org/jpf/Main.java
+++
@@ -1,6 +0,0 @@
-package org.jpf;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
